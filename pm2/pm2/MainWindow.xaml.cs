30f91e942bafc2f5a0551872cd2e078e242d0cd2
ellesse04 <38053150+ellesse04@users.noreply.github.com>
2023-03-12 23:24:41 +0400

    Add files via upload

---
+++ pm2/pm2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pm2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] sdata = DataBox.Text.Split(new char[] { ' ', '\n' });
+            List<int> data = sdata.Select(int.Parse).ToList();
+
+            Window1 w1 = new Window1(data);
+            w1.Show();
+            Close();
+        }
+        private void validation(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ pm2/pm2/Window1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pm2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public List<List<int>> toMatrix(List<int> data)
+        {
+            data.RemoveAt(0);
+            List<List<int>> G = new List<List<int>>();
+            int size = Convert.ToInt32(Math.Sqrt(data.Count()));
+            for (int i = 0; i < size; i++)
+                G.Add(new List<int> (size));
+
+            int cnt = 0;
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    G[i].Add(data[cnt++]);
+
+            return G;
+        }
+
+        public struct Pair
+        {
+            public int a;
+            public int b;
+        }
+
+        public List<Pair> toList(List<int> data)
+        {
+            int n = data[0];
+            int m = data[1];
+            data.RemoveAt(0);
+            data.RemoveAt(0);
+            
+            List<Pair> G = new List<Pair>(0);
+            for(int i = 0; i < data.Count(); i=i+2)
+            {
+                Pair a = new Pair();
+                a.a = data[i];
+                a.b = data[i+1];
+
+                G.Add(a);
+            }
+
+            return G;
+        }
+
+        public Window1(List<int> data)
+        {
+            InitializeComponent();
+            List<Pair> g = toList(data);
+            List<List<int>> gg = toMatrix(data);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+    }
+}
